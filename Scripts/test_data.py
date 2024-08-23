9a93ef876027737d9ce6ac08206d2410c05020e2
yordanossimegnew <yordaanossimegnew@gmial.com>
2024-08-23 21:55:59 +0300

    test_data

---
+++ Scripts/test_data.py
@@ -0,0 +1,22 @@
+# sample prediction json file
+{
+    "data": [
+        {
+            "Name": "The Legend of Zelda",
+            "Platform": "Switch",
+            "Year_of_Release": "2017",
+            "Genre": "Action",
+            "Publisher": "Nintendo",
+            "NA_Sales": 0.1,
+            "EU_Sales": 0.2,
+            "JP_Sales": 0.3,
+            "Other_Sales": 0.1,
+            "Critic_Score": "80",
+            "Critic_Count": 50,
+            "User_Score": 8.5,
+            "User_Count": 1000,
+            "Developer": "Nintendo",
+            "Rating": "E"
+        }
+    ]
+}
